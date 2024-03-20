a9387f17664f19bcec828f0d1da10ba4490a9a6c
fathindifa26 <112231972+fathindifa26@users.noreply.github.com>
2024-03-20 14:39:07 +0700

    Add files via upload

---
+++ directory.py
@@ -0,0 +1,4 @@
+import os
+directory = os.environ.get('DIRECTORY', '/Users/fathindifarobbani/Documents/Difa/Synapsis/')
+photo_booth = "/Users/fathindifarobbani/Pictures/Photo Booth Library/Pictures"
+
---
+++ presensi_pengguna_lama.py
@@ -0,0 +1,32 @@
+import csv
+import os
+from datetime import datetime
+import directory
+
+directory = directory.directory
+
+
+def write_csv_header():
+    file = os.path.join(directory, 'data_presensi.csv')
+    if not os.path.exists(file):
+        with open(file, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Nama', 'Waktu'])
+            print("Header CSV telah ditulis.")
+
+def add_new_entry(name):
+    if not os.path.exists(file):
+        write_csv_header()
+    with open(file, mode='a', newline='') as file:
+        fieldnames = ['Nama', 'Waktu']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow({'Nama': name, 'Waktu': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+
+def execute_database(name):
+    add_new_entry(name)
+
+# Contoh penggunaan
+if __name__ == "__main__":
+    # Contoh penggunaan fungsi execute_database() dengan variabel name
+    name = "Fathin Difa Robbani"  # Atur nama sesuai kebutuhan
+    execute_database(name)
